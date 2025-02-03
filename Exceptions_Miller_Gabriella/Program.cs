c9af6f34cb4fd504e2d0b3d3d82f0197d8d495ab
Gabby <gabbie10112005@gmail.com>
2025-02-03 15:08:58 -0600

    a

--- Exceptions_Miller_Gabriella/Program.cs
+++ Exceptions_Miller_Gabriella/Program.cs
@@ -4,7 +4,71 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            float myFloat = 65.4f;
+            float myOtherFloat = 0.0f;
+            float result = 0f;
+
+            Random rand = new Random();
+            int myInt = rand.Next(2, 30);
+
+            try
+            {
+                result = Divide(myFloat, myOtherFloat);
+            } 
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Please enter a number other than zero");
+                myOtherFloat = (float)Convert.ToDouble(Console.ReadLine());
+                
+                try
+                {
+                    result = Divide(myFloat, myOtherFloat);
+                }
+                catch (Exception e2)
+                {
+                    Console.WriteLine(e2.Message);
+                }
+            }
+            finally
+            {
+                Console.WriteLine("Calculations comepletes with a result of " + result);
+            }
+
+            try
+            {
+                CheckAge(myInt);
+            }
+            catch
+            {
+                Console.WriteLine("You are not old enough! Be gone, minor!");
+            }
+        }
+
+
+
+        static float Divide(float x, float y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            else
+            {
+                return x / y;
+            }
+        }
+
+        static void CheckAge(int age)
+        {
+            if(age >= 17)
+            {
+                Console.WriteLine($"You are {age}, you can play mature games!");
+            }
+            else
+            {
+                throw new DivideByZeroException();
+            }
         }
     }
 }
